427d00bad74ae9e3aa1279818c869605b6e01970
Sergio Phillippe <sergiophillippe@gmail.com>
2026-05-11 15:58:53 -0300

    testes

---
+++ arrays.py
@@ -0,0 +1,9 @@
+notas = [9, 7, 10, 8.5]
+
+media = 0
+for nota in notas:
+    media += nota
+
+media /= 4
+
+print(f'A média é {media}')
---
+++ teste-condicional.py
@@ -0,0 +1,10 @@
+print('Bem-vindo do Detran!')
+print('Este é o teste')
+print()
+idade = int(input('Digite a sua idade: '))
+
+if idade >= 18 :
+    print('Pode dirigir!')
+else:
+    print('Não pode dirigir!')
+print('Acabou!')
